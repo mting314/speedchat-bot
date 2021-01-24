c13303c84f78bdf0e326237c73d57a612ef6538f
mting314 <57047134+mting314@users.noreply.github.com>
2021-01-23 20:51:13 -0800

    improve japanese searching

--- speedchat_bot/japanese.py
+++ speedchat_bot/japanese.py
@@ -14,7 +14,7 @@ class Japanese(commands.Cog):
         self.j = jisho.Jisho()
 
     @commands.command(help="Display useful info for a word")
-    async def search(self, ctx, kanji: str):
+    async def jsearch(self, ctx, kanji: str):
         status = await ctx.send(f"Searching for {kanji}")
         response_message = ""
         try:
@@ -23,14 +23,12 @@ class Japanese(commands.Cog):
             entry = result['data'][0]
             jword = entry['japanese'][0]['word']
             reading = entry['japanese'][0]['reading']
-            # url = result['uri']
 
-            # response_message += f'{jword}({reading}) {url}'
             response_message += f'{jword}({reading})'
 
             for index, sense in enumerate(entry['senses']):
                 response_message += f'\n{index + 1}. ' + ', '.join(sense['english_definitions'])
-        except TypeError as e:
+        except (TypeError, KeyError) as e:
             print(e)
             await status.edit(content="Sorry, I could not find that.")
             return
@@ -40,31 +38,22 @@ class Japanese(commands.Cog):
     @commands.command(help="Look up the stroke order for a word")
     async def stroke(self, ctx, word: str):
         status = await ctx.send(f"Searching for {word}'s stroke order.")
-        try:
-            for kanji in word:
-                await ctx.send(self.j.searchForKanji(kanji)['strokeOrderGifUri'])
-        except TypeError as e:
-            print(e)
-            await status.edit(content="Sorry, I could not find that.")
-            return
+        sent = False
 
-        # await status.edit(content=response_message)
+        # If we looked up hepburn
+        if word.isalpha():
+            result = self.j.searchForPhrase(word)
+            entry = result['data'][0]
+            search_value = entry['japanese'][0]['word']
+        else:
+            search_value = word
+
+        for kanji in search_value:
+            kanji_result = self.j.searchForKanji(kanji)
+            if 'strokeOrderGifUri' in kanji_result:
+                await ctx.send(kanji_result['strokeOrderGifUri'])
+                sent = True
 
 
-    @commands.command(help="Test again real")
-    async def test(self, ctx, kanji: str):
-        await ctx.send(f"Mine:")
-        status = await ctx.send(f"Searching for {kanji}.")
-        # try:
-        result = self.j.searchForKanji(kanji)
-        # for benis in result:
-        await ctx.send(str(result)[:1999])
-        # response_message = json.dumps(self.j.searchForWord(kanji), indent=4)[:1999]
-        # response_message2 = json.dumps(self.j.searchForWord(kanji), indent=4)[1999:3999]
-        # except TypeError as e:
-        #     print(e)
-        #     await status.edit(content="Sorry, I could not find that.")
-        #     return
-
-        # await status.edit(content=response_message)
-        # await ctx.send(response_message2)
+        if not sent:
+            await status.edit("Sorry, I couldn't find that.")
