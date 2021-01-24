4178af9f0600e46b06f974f16455417dd02e8b7f
mting314 <57047134+mting314@users.noreply.github.com>
2021-01-24 01:13:03 -0800

    add decorator check to DM if first time

--- speedchat_bot/perms.py
+++ speedchat_bot/perms.py
@@ -2,11 +2,17 @@ import os
 from discord.ext import commands
 
 
-# you may be an admin...
-def is_admin():
-    return commands.check(lambda ctx: [role for role in ctx.author.roles if role.permissions.administrator])
-
-
 # ...but are you me?
 def is_owner():
     return commands.check(lambda ctx: str(ctx.author) == os.getenv('OWNER'))
+
+# check if this is first time user interacting with bot
+def first_time():
+    async def predicate(ctx):
+        if not ctx.author.dm_channel:
+            channel = await ctx.author.create_dm()
+            messages = await channel.history().flatten()
+            if not any([(message.content.startswith("Hi, thanks for using this bot") and message.author.bot) for message in messages]):
+                await ctx.author.send("Hi, thanks for using this bot! Just so you know, you can also perform all the same commands in this DM channel, if you don't want to spam other people in a server.")
+        return True
+    return commands.check(predicate)
